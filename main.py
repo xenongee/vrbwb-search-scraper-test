0de08d3e40b385f3db135607167edcd7ca64e1d0
Gleb <rublgleb@gmail.com>
2026-01-19 08:12:38 +0300

    feat: some fixes add excel export

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import random
 import time
+import pandas
 from urllib.parse import urlencode
 from playwright.sync_api import sync_playwright
 from playwright_stealth import Stealth
 
-HEADLESS = False
+HEADLESS = True
 BASE_URL = "https://www.wildberries.ru"
 SEARCH_PARAMS = {
-    "page": 1,
     "sort": "popular",
     "search": "пальто из натуральной шерсти",
-    "f14177451": "15000203", # Made by: Russia
-    "priceU": "000;1000000"
 }
 MADEBY = "Россия"
 SEARCH_URL = f"{BASE_URL}/catalog/0/search.aspx?{urlencode(SEARCH_PARAMS)}"
+MAX_PRODUCTS_COUNT = 100
+# MAX_PRODUCTS_COUNT = 5 # for testing
 LOCATES = {
     # antibot
     "antibot_title": ".support-title",
@@ -47,11 +47,8 @@ LOCATES = {
     "product_seller_url": "a[class*='sellerInfoButtonLink']",
 
     # Product sizes
-    "product_size_btn_more": "li[class*='sizesListItemMore'] button",
-    "product_sizes_list": "ul[class*='sizesList']",
-    "product_size_item": "li[class*='sizesListItem']",
-    "product_size_main_text": "span[class*='sizesListSize--']",
-    "product_size_caption_text": "span[class*='sizesListSizeRu']",
+     "product_size_btn_more": "li[class*='sizesListItemMore'] button",
+     "product_sizes_list": "ul[class*='sizesList']",
 
     #Product stock and reviews
     "product_stock_count": "[class*='qtyTrigger'] span",
@@ -59,8 +56,24 @@ LOCATES = {
     "product_rating": ".user-opinion__rating-numb",
     "product_review_count": ".user-opinion__text",
 }
-# MAX_PRODUCTS_COUNT = 100
-MAX_PRODUCTS_COUNT = 1 # for testing
+PRICE_TYPE = "wallet_price" # wallet_price or final_price
+COLUMNS_MAP = {
+    "url": "Ссылка на товар",
+    "article": "Артикул",
+    "name": "Название",
+    PRICE_TYPE: "Цена",
+    "description": "Описание",
+    "images_urls": "Ссылки на изображения",
+    "characteristics": "Все характеристики",
+    "seller_name": "Название селлера",
+    "seller_url": "Ссылка на селлера",
+    "sizes": "Размеры товара",
+    "stock_count": "Остатки по товару (число)",
+    "rating": "Рейтинг",
+    "review_count": "Количество отзывов",
+}
+FILE_NAME = "wb-scraped.xlsx"
+FILE_NAME_FILTERED = "wb-scraped-filtered.xlsx"
 
 def check_catalog_is_loaded(page):
     timeout = 15000
@@ -142,9 +155,7 @@ def extract_rating(text):
     except:
         return 0.0
 
-def parse_product_page(page, url):
-    print(f"Parsing product page: {url}")
-
+def scrape_product_page(page, url):
     product_data = {
         "url": url,
         "article": "",
@@ -153,7 +164,7 @@ def parse_product_page(page, url):
         "final_price": 0,
         "description": "",
         "images_urls": "",
-        "characteristics": "",
+        "characteristics": {},
         "seller_name": "",
         "seller_url": "",
         "sizes": "",
@@ -165,23 +176,28 @@ def parse_product_page(page, url):
     # Load page and check loading
     try:
         page.goto(url, wait_until="domcontentloaded")
-        page.wait_for_selector(LOCATES["product_title"], timeout=10000)
+        page.wait_for_selector(LOCATES["product_title"], timeout=15000)
     except Exception as e:
         print(f"Error loading product page: {url}. Details: {e}")
         return None
 
     # Characteristics
     try:
-        # Fail Details: Page.wait_for_selector: Timeout 10000ms exceeded.
-        # не всегда открывется модальное окно с характеристиками, хотя кнопку видно в браузере... исполняется до загрузки страницы?
         characteristics_btn_locator = page.locator(LOCATES["product_characteristics_btn"]).first
         if characteristics_btn_locator.is_visible():
-            characteristics_btn_locator.click(force=True, timeout=10000)
+            characteristics_btn_locator.scroll_into_view_if_needed()
             time.sleep(0.5)
-            page.wait_for_selector(f"{LOCATES['product_characteristics_modal']} table", timeout=10000)
+            characteristics_btn_locator.click(force=True)
+            try:
+                page.locator(f"{LOCATES['product_characteristics_modal']} table").first.wait_for(timeout=5000)
+            except: pass
 
+    except Exception as e:
+        print(f"Error: Failed to open characteristics modal: {url}. Details: {e}")
+
+    try:
         tables = page.locator(f"{LOCATES['product_characteristics_modal']} table").all()
-        specs = {}
+        data = {}
 
         for table in tables:
             rows = table.locator("tr").all()
@@ -193,20 +209,15 @@ def parse_product_page(page, url):
                     key = clean_text(key_el.inner_text())
                     val = clean_text(val_el.inner_text())
                     if key and val:
-                        specs[key] = val
-
-        country = specs.get("Страна производства", "").lower()
-        if MADEBY.lower() not in country:
-            print(f"Warning: Product made by other country ({country})")
-            return None
+                        data[key] = val
 
         description_locator = page.locator(LOCATES["product_description"]).first
         if description_locator.is_visible():
             product_data["description"] = clean_text(description_locator.inner_text())
 
-        product_data["characteristics"] = specs
+        product_data["characteristics"] = data
     except Exception as e:
-        print(f"Error: Failed to open characteristics modal or scrape data from it: {url}. Details: {e}")
+        print(f"Error: Failed to scrape product characteristics: {url}. Details: {e}")
 
     # Title
     try:
@@ -220,21 +231,18 @@ def parse_product_page(page, url):
     except Exception as e:
         print(f"Error: Failed to parse product article: {url}. Details: {e}")
 
-    # Wallet Price
+    # Prices
     try:
         wallet_price_locator = page.locator(LOCATES["product_wallet_price"]).first
+        final_price_locator = page.locator(LOCATES["product_final_price"]).first
+
         if wallet_price_locator.is_visible():
             product_data["wallet_price"] = extract_digits(wallet_price_locator.inner_text())
-    except Exception as e:
-        print(f"Error: Failed to parse product wallet price: {url}. Details: {e}")
 
-    # Final Price
-    try:
-        final_price_locator = page.locator(LOCATES["product_final_price"]).first
         if final_price_locator.is_visible():
             product_data["final_price"] = extract_digits(final_price_locator.inner_text())
     except Exception as e:
-        print(f"Error: Failed to parse product final price: {url}. Details: {e}")
+        print(f"Error: Failed to parse product price: {url}. Details: {e}")
 
     # Images
     try:
@@ -251,7 +259,7 @@ def parse_product_page(page, url):
     except Exception as e:
         print(f"Error: Failed to parse product images: {url}. Details: {e}")
 
-    # Seller Name
+    # Seller
     try:
         seller_default_locator = page.locator(LOCATES["product_seller_name"]).first
         seller_user_locator = page.locator(LOCATES["product_seller-user_name"]).first
@@ -261,48 +269,32 @@ def parse_product_page(page, url):
         elif seller_user_locator.is_visible():
             product_data["seller_name"] = f"{seller_user_locator.inner_text()}"
             # product_data["seller_name"] = f"{seller_user_locator.inner_text()} (Покупатель WB)"
-    except Exception as e:
-        print(f"Error: Failed to parse product seller name: {url}. Details: {e}")
 
-    # Seller URL
-    try:
         seller_url_locator = page.locator(LOCATES["product_seller_url"]).first
         if seller_url_locator.is_visible():
             seller_href = seller_url_locator.get_attribute("href")
             if seller_href:
                 product_data["seller_url"] = f"{BASE_URL}{seller_href}"
     except Exception as e:
-        print(f"Error: Failed to parse product seller URL: {url}. Details: {e}")
+        print(f"Error: Failed to parse product seller name or seller url: {url}. Details: {e}")
 
     # Sizes
     try:
         product_size_btn_more_locator = page.locator(LOCATES["product_size_btn_more"]).first
         if product_size_btn_more_locator.is_visible():
-            product_size_btn_more_locator.click()
+            product_size_btn_more_locator.click(force=True)
             time.sleep(0.5)
 
         sizes_list = []
+        items = page.locator(f"{LOCATES['product_sizes_list']} button").all()
 
-        size_items = page.locator(f"{LOCATES['product_sizes_list']} {LOCATES['product_size_item']} button").all()
-        for item in size_items:
-            main_span = item.locator(LOCATES["product_size_main_text"]).first
-            caption_span = item.locator(LOCATES["product_size_caption_text"]).first
+        for item in items:
+            text = item.inner_text().replace("\n", " RU: ").strip()
+            sizes_list.append(clean_text(text))
 
-            size_text=""
-            if main_span.is_visible():
-                main_text = clean_text(main_span.inner_text())
-                size_text = main_text
-
-                if caption_span.is_visible():
-                    caption_text = clean_text(caption_span.inner_text())
-                    size_text += f" ({caption_text})"
-
-            if size_text:
-                sizes_list.append(size_text)
-
-        product_data["sizes"] = ", ".join(sizes_list)
+        product_data["sizes"] = ", ".join(list(set(sizes_list)))
     except Exception as e:
-        print(f"Sizes error: {e}")
+        print(f"Error: Failed to parse product sizes: {url}. Details: {e}")
 
     # Stock Count
     try:
@@ -328,6 +320,56 @@ def parse_product_page(page, url):
 
     return product_data
 
+def save_to_excel_simple(filename, data_list, columns_map):
+    if not data_list:
+        print("No data to save!")
+        return
+
+    dataframe = pandas.DataFrame(data_list)
+
+    def clean_cell(data):
+        if isinstance(data, dict):
+            lines = []
+            for key, value in data.items():
+                line = f"{key}: {value}"
+                lines.append(line)
+            return "\n".join(lines)
+
+        if isinstance(data, list):
+            return ", ".join(map(str, data))
+
+        return data
+
+    dataframe = dataframe.map(clean_cell)
+
+    dataframe = dataframe[list(columns_map.keys())]
+    dataframe = dataframe.rename(columns=columns_map)
+
+    dataframe.to_excel(filename, index=False)
+    print(f"File saved: {filename} ({len(dataframe)} rows)")
+
+# Filter products based on criteria
+def is_specific_product(item):
+    characteristic_items = item.get("characteristics", {})
+
+    if not isinstance(characteristic_items, dict):
+        return False
+
+    country = ""
+    for key, value in characteristic_items.items():
+        if "страна" in key.lower():
+            country = value
+            break
+
+    current_rating = float(item.get("rating", 0))
+    current_price = int(item.get(PRICE_TYPE, 0))
+
+    return (
+        current_rating >= 4.5 and
+        current_price < 10000 and
+        MADEBY.lower() in country.lower()
+    )
+
 def main():
     print(f"Work with {SEARCH_URL}")
 
@@ -341,32 +383,42 @@ def main():
             ### Check antibot
             if check_catalog_is_loaded(page):
                 ### Collect links
-                # links = collect_products_links(page, MAX_PRODUCTS_COUNT)
-                # print(f"Collected {len(links)} product links: {links[:MAX_PRODUCTS_COUNT]}")
-                links = [
-                    "https://www.wildberries.ru/catalog/141175340/detail.aspx?size=239205585",
-                    # "https://www.wildberries.ru/catalog/230636222/detail.aspx",
-                    # "https://www.wildberries.ru/catalog/689821627/detail.aspx",
-                ]
+                links = collect_products_links(page, MAX_PRODUCTS_COUNT)
+                print(f"Collected {len(links)} product links: {links[:MAX_PRODUCTS_COUNT]}")
 
                 ### Parse products from links
                 products_data = []
-                for link in links[:MAX_PRODUCTS_COUNT]:
-                    prod_data = parse_product_page(page, link)
-                    if prod_data:
-                        products_data.append(prod_data)
-                        print(f"Scraped {len(products_data)} products data:")
-                        for pd in products_data:
-                            print(pd)
+                for i, link in enumerate(links[:MAX_PRODUCTS_COUNT]):
+                    print(f"[{i+1}/{len(links)}] Product: {link}")
+                    data = scrape_product_page(page, link)
+                    if data:
+                        products_data.append(data)
+
                     time.sleep(random.uniform(1, 3)) # Delay between product page requests
 
             else:
-                print("Catalog did not load properly")
+                print("Exiting...")
+                return
 
         except Exception as e:
             print(f"Error: {e}")
         finally:
             browser.close()
+
+    ### Save results to Excel
+    if products_data:
+        save_to_excel_simple(FILE_NAME, products_data, COLUMNS_MAP)
+
+        filtered_data = []
+        for item in products_data:
+            if is_specific_product(item):
+                filtered_data.append(item)
+
+        print(f"Filtered products: {len(filtered_data)}")
+        if filtered_data:
+            save_to_excel_simple(FILE_NAME_FILTERED, filtered_data, COLUMNS_MAP)
+        else:
+            print("Filtered list is empty, no file created.")
 
 if __name__ == "__main__":
     main()
